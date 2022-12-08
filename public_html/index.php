5bf3f85550e17842825bdd52049e53607d12ae2a
Borisov Andriy <BorisovAndriy@gmail.com>
2022-12-08 17:20:59 +0200

    Php test

---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Короткий опис файлу
+ *
+ * @author Borisov Andriy
+ * @version B 0.1
+ * @todo
+ * @example http://tracing-patterns.loc/
+ * @copyright Copyright (c) 2022, Borisov Andriy
+ */
+?>
